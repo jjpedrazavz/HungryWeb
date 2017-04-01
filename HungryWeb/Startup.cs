6002fa28878b4a00f6c61a881d6c67fcf8f97b11
admin1 <admin1@DESKTOP-QU97L6I>
2017-04-01 13:42:28 -0600

    Agregar archivos de proyecto.

---
+++ HungryWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HungryWeb.Startup))]
+namespace HungryWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
